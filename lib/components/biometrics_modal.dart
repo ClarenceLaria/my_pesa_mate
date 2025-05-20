4d2f944cfd38efa9a9f6254455ead876208a2289
ClarenceLaria <clarencelaria@gmail.com>
2025-05-20 18:47:00 +0300

    Biometrics modal updates

--- lib/components/biometrics_modal.dart
+++ lib/components/biometrics_modal.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:kurerefinancialplanner_app/auth/auth_helper.dart';
 import 'package:kurerefinancialplanner_app/components/face_id.dart';
 import 'package:kurerefinancialplanner_app/components/fingerprint_modal.dart';
+import 'package:local_auth/local_auth.dart';
 
 class BiometricsModal extends StatefulWidget {
   const BiometricsModal({super.key});
@@ -10,16 +12,41 @@ class BiometricsModal extends StatefulWidget {
 }
 
 class _BiometricsModalState extends State<BiometricsModal> {
+  final LocalAuthentication auth = LocalAuthentication();
+
+  Future<List<BiometricType>> _getAvailableBiometrics() {
+    return auth.getAvailableBiometrics();
+  }
+
+  final available = LocalAuthService().auth.getAvailableBiometrics();
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        FingerprintModal(),
-        FaceId(),
+    return FutureBuilder(
+      future: _getAvailableBiometrics(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        }
 
-      ],
+        if (!snapshot.hasData || snapshot.data == null) {
+          return const Text("Error checking biometrics");
+        }
+
+        final biometrics = snapshot.data!;
+        final hasFingerprint = biometrics.contains(BiometricType.fingerprint);
+        final hasFace = biometrics.contains(BiometricType.face);
+
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            if (hasFingerprint && hasFace) ...[
+              const FingerprintModal(),
+              const FaceId()
+            ] else const FingerprintModal(),
+          ],
+        );
+      },
     );
   }
 }
